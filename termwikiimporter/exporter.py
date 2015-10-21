9ad03204fda37d334bb455d42a51667aae0961ac
Børre Gaup <albbas@gmail.com>
2015-10-21 13:52:45 +0000

    Don't overwrite pages with the same name

--- termwikiimporter/exporter.py
+++ termwikiimporter/exporter.py
@@ -27,9 +27,34 @@ def write_to_termwiki():
     site = mwclient.Site('gtsvn.uit.no', path='/termwiki/')
     site.login('SDTermImporter', password)
 
-    for wikifile in args.wikifiles:
-        for wikipage in etree.parse(wikifile).xpath('.//page'):
-            page = site.Pages[wikipage.get('title')]
-            page.save(wikipage.find('./content').text,
-                      summary='Import from ' + wikifile.replace('.xml', '.xlsx'))
+    page_titles = collections.defaultdict(int)
+    all_pages = etree.Element('all_pages')
 
+    for wikifile in args.wikifiles:
+        wikitree = etree.parse(wikifile)
+        for wikipage in wikitree.xpath('.//page'):
+            all_pages.append(wikipage)
+            page_titles[wikipage.get('title')] += 1
+
+    for page_title, count in page_titles.items():
+        pages = all_pages.xpath('.//page[@title="' + page_title + '"]')
+        print(page_title, count, len(pages))
+        new_page_title = page_title
+        counter = 1
+        while len(pages) > 0 and counter <= count + 1:
+            print('testing page', new_page_title)
+            site_page = site.Pages[new_page_title]
+            site_text = site_page.text()
+            if site_text != '':
+                print('\t removing from list', new_page_title)
+                for page in pages:
+                        for page in pages:
+                            if site_text == page.find('./content').text:
+                                pages.remove(page)
+            else:
+                print('\t adding content', new_page_title)
+                site_page.save(pages.pop().find('./content').text,
+                                summary='New import')
+            new_page_title = page_title + '_' + str(counter)
+            counter += 1
+        print('pages len', len(pages))

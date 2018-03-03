7693f57214288059b27a2ec93e65a186ee186bca
Børre Gaup <albbas@gmail.com>
2018-03-03 20:39:56 +0000

    Turn Concept templates to yet another xml format

--- termwikiimporter/dump2xml.py
+++ termwikiimporter/dump2xml.py
@@ -1,54 +1,68 @@
 # -*- coding: utf-8 -*-
-""""""
+"""Turn Concept templates found in termwiki dump.xml file to xml."""
 
-class TermWiki(object):
+import os
+from lxml import etree
+
+from termwikiimporter import read_termwiki
+
+
+def concept2xml(concept, general):
+    for key, value in concept['concept'].items():
+        if key == 'collection':
+            for collection in concept['concept']['collection']:
+                etree.SubElement(general, 'collection').text = collection
+        else:
+            etree.SubElement(general, key).text = value
+
+
+def keys2key_xml(concept, keys, general):
+    for concept_info in concept[keys]:
+        info = etree.SubElement(general, keys[:-1])
+        for key, value in concept_info.items():
+            etree.SubElement(info, key).text = value
+
+
+def concept_page2xml(concept):
+    """Turn a TermWiki concept into xml.
+
+    Arguments:
+        concept (dict): contains the info found inside a TermWiki
+            Concept template.
+    """
+    concept_xml = etree.Element('concept')
+
+    concept2xml(concept, concept_xml)
+    for keys in ['concept_infos', 'related_expressions', 'related_concepts']:
+        keys2key_xml(concept, keys, concept_xml)
+
+    return concept_xml
+
+
+def dump2contents():
+    tree = etree.parse(
+        os.path.join(os.getenv('GTHOME'), 'words/terms/termwiki/dump.xml'))
+    namespaces = {'m': 'http://www.mediawiki.org/xml/export-0.10/'}
+
+    for page in tree.getroot().xpath('.//m:page', namespaces=namespaces):
+        text = page.find('.//m:text', namespaces=namespaces)
+        if (text is not None and
+                text.text is not None and '{{Concept' in text.text):
+            concept = read_termwiki.parse_termwiki_concept(text.text)
+            concept['concept']['title'] = page.find(
+                './/m:title', namespaces=namespaces).text
+
+            yield concept
+
+
+def dump2xml():
     """Read concepts from the dump.xml file."""
+    pages = etree.Element('concepts')
 
-    def __init__(self):
-        self.pages = etree.Element('pages')
+    for concept in dump2contents():
+        pages.append(concept_page2xml(concept))
 
-    def termwiki2xml(self, concept):
-        page = etree.SubElement(self.pages, 'page')
-        general = etree.SubElement(page, 'general')
-
-        for key, value in concept['concept'].items():
-            if key == 'collection':
-                for collection in concept['concept']['collection']:
-                    etree.SubElement(general, 'collection').text = collection
-            else:
-                etree.SubElement(general, key).text = value
-
-        for concept_info in concept['concept_infos']:
-            info = etree.SubElement(general, 'concept_info')
-            for key, value in concept_info.items():
-                etree.SubElement(info, key).text = value
-
-        for related_expression in concept['related_expressions']:
-            expression = etree.SubElement(general, 'expression')
-            for key, value in related_expression.items():
-                etree.SubElement(expression, key).text = value
-
-        for related_concept in concept['related_concepts']:
-            related_conc = etree.SubElement(general, 'related_concept')
-            for key, value in related_concept.items():
-                etree.SubElement(related_conc, key).text = value
-
-    def read_concepts(self):
-        tree = etree.parse(
-            os.path.join(os.getenv('GTHOME'), 'words/terms/termwiki/dump.xml'))
-        namespaces = {'m': 'http://www.mediawiki.org/xml/export-0.10/'}
-
-        for page in tree.getroot().xpath('.//m:page', namespaces=namespaces):
-            text = page.find('.//m:text', namespaces=namespaces)
-            if (text is not None and
-                    text.text is not None and '{{Concept' in text.text):
-                concept = read_termwiki.parse_termwiki_concept(text.text)
-
-                concept['concept']['title'] = page.find(
-                    './/m:title', namespaces=namespaces).text
-
-                self.termwiki2xml(concept)
-
-        print(etree.tostring(self.pages, encoding='unicode', pretty_print=True))
+    return pages
 
 
+print(etree.tostring(dump2xml(), encoding='unicode', pretty_print=True))

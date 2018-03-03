08d62c4b080909b3d663ee998c53f82599b17c7b
Børre Gaup <albbas@gmail.com>
2018-03-03 20:41:29 +0000

    Improve dump

--- termwikiimporter/dump2xml.py
+++ termwikiimporter/dump2xml.py
@@ -2,12 +2,18 @@
 """Turn Concept templates found in termwiki dump.xml file to xml."""
 
 import os
+import sys
 from lxml import etree
+from collections import defaultdict
 
 from termwikiimporter import read_termwiki
 
 
+counter = defaultdict(int)
+
+
 def concept2xml(concept, general):
+    """Set the main elements of a concept element."""
     for key, value in concept['concept'].items():
         if key == 'collection':
             for collection in concept['concept']['collection']:
@@ -16,14 +22,31 @@ def concept2xml(concept, general):
             etree.SubElement(general, key).text = value
 
 
-def keys2key_xml(concept, keys, general):
-    for concept_info in concept[keys]:
-        info = etree.SubElement(general, keys[:-1])
+def keys2key_xml(concept_info, element_name, concept_xml):
+    """Turn template attributes into xml elements.
+
+    Arguments:
+        concept (dict): dict containing info TermWiki template attributes
+        element_name (str): name of the TermWiki template
+        concept2xml (etree.Element): the Concept element
+    """
+    if not (concept_info.get('sanctioned') and
+            concept_info['sanctioned'] == 'False'):
+        info = etree.SubElement(concept_xml, element_name)
         for key, value in concept_info.items():
             etree.SubElement(info, key).text = value
 
 
-def concept_page2xml(concept):
+def contains_sami(concept_xml):
+    uff = set.intersection(
+        set(['se', 'sma', 'smj', 'smn', 'sms']),
+        {lang.text
+         for lang in concept_xml.xpath('./related_expression/language')})
+
+    return len(uff)
+
+
+def concept_page2xml(concept, pages):
     """Turn a TermWiki concept into xml.
 
     Arguments:
@@ -34,12 +57,20 @@ def concept_page2xml(concept):
 
     concept2xml(concept, concept_xml)
     for keys in ['concept_infos', 'related_expressions', 'related_concepts']:
-        keys2key_xml(concept, keys, concept_xml)
+        for concept_info in concept[keys]:
+            keys2key_xml(concept_info, keys[:-1], concept_xml)
 
-    return concept_xml
+    if contains_sami(concept_xml):
+        counter['sami'] += 1
+        pages.append(concept_xml)
 
 
 def dump2contents():
+    """Turn concept pages found in dump.xml to a dict.
+
+    Yields:
+        dict: dump.xml concept turned into a dict
+    """
     tree = etree.parse(
         os.path.join(os.getenv('GTHOME'), 'words/terms/termwiki/dump.xml'))
     namespaces = {'m': 'http://www.mediawiki.org/xml/export-0.10/'}
@@ -51,18 +82,22 @@ def dump2contents():
             concept = read_termwiki.parse_termwiki_concept(text.text)
             concept['concept']['title'] = page.find(
                 './/m:title', namespaces=namespaces).text
-
+            counter['concepts'] += 1
             yield concept
 
 
 def dump2xml():
-    """Read concepts from the dump.xml file."""
+    """Read concepts from the dump.xml file.
+
+    Only save terms where at least one related expression is sanctioned.
+    """
     pages = etree.Element('concepts')
 
     for concept in dump2contents():
-        pages.append(concept_page2xml(concept))
+        concept_page2xml(concept, pages)
 
     return pages
 
 
 print(etree.tostring(dump2xml(), encoding='unicode', pretty_print=True))
+print(counter, file=sys.stderr)

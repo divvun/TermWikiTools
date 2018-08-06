77c795024ee330b815a93ce4f91d2675218fa275
Børre Gaup <albbas@gmail.com>
2018-08-06 20:26:34 +0000

    Send the first e element into the e2tuple functin

--- termwikiimporter/test/test_dicts2wiki.py
+++ termwikiimporter/test/test_dicts2wiki.py
@@ -123,7 +123,7 @@ class TestDicts(unittest.TestCase):
                 restriction='i negative sammenhenger',
                 translations=self.translations,
                 examples=self.examples)])
-        got = dicts2wiki.e2tuple(self.dictxml, 'sme', 'nob')
+        got = dicts2wiki.e2tuple(self.dictxml.find('.//e'), 'sme', 'nob')
 
         self.assertTupleEqual(want, got)
 

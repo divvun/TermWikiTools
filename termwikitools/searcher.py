cc3954d9f8c00dcab655a0b77492e6fe5ab000c4
Børre Gaup <albbas@gmail.com>
2024-10-21 00:01:57 +0200

    Masse endringer i søk

--- termwikitools/searcher.py
+++ termwikitools/searcher.py
@@ -19,13 +19,24 @@
 """Search dump."""
 
 import collections
+import json
 
 import click
 
 from termwikitools import bot
-from termwikitools.handler_common import LANGUAGES
+
+# For hver av artiklene i inputfila, så vil jeg:
+# 1. Sjekke om termene i artikkelen finnes i søkeindeksen
+# 2. Hvis det finnes ett treff, vis hvilken info fra artikkelen i inputfila
+#    som legges til treffartikkelen.
 
 
+# To modi: For hver av artiklene i inputfila, så vil jeg:
+# 1. Sjekke om termene i artikkelen finnes i søkeindeksen
+# modus 1: skriv ut titlene på treffartiklene
+# modus 2: for artkikler med treff, 1. velg hvilken tittel man vil
+# flette inputartikkelen i, 2. erstatt den valgte inputartikkelen med
+# den flettede artikkelen.
 def make_search_index():
     """Make a search index."""
     dump_handler = bot.DumpHandler()
@@ -36,56 +47,78 @@ def make_search_index():
     return search_index
 
 
+def get_searches(infile):
+    with click.open_file(infile, "r") as f:
+        my_json = json.load(f)
+        for concept in my_json["concepts"]:
+            for related_expression in concept["related_expressions"]:
+                yield related_expression["expression"], related_expression[
+                    "language"
+                ], concept["title"]
+
+
 @click.command()
-@click.option("--outfile", default="termwiki.tsv", help="Output file.")
-@click.argument("search_language", type=click.Choice(list(LANGUAGES.keys())), nargs=1)
-@click.argument("searches", nargs=-1)
-def main(outfile, search_language, searches):
+@click.option("--infile", help="Input file")
+# @click.option("--outfile", default="termwiki.tsv", help="Output file")
+# @click.argument("search_language", type=click.Choice(list(LANGUAGES.keys())), nargs=1)
+# @click.argument("searches", nargs=-1)
+def main(infile):
     """Search dump."""
-    termwiki_language = LANGUAGES[search_language]
+    # termwiki_language = LANGUAGES[search_language]
     search_index = make_search_index()
 
-    old_to_new_langs = {v: k for k, v in LANGUAGES.items()}
-    search_terms = sorted(
-        {
-            search.lower().strip().replace(")", "").replace(")", "").replace(":", "")
-            for c_search in searches
-            for search in c_search.split()
-        }
-    )
-    results = [
-        {
-            language: (
-                f'{", ".join(termwiki_page.get_terms(language))}'
-                f" {termwiki_page.get_definition(language)}"
-            ).strip()
-            for termwiki_page in termwiki_pages
-            for language in termwiki_page.get_languages()
-        }
-        for termwiki_pages in [
-            search_index[search] for search in search_terms if search in search_index
-        ]
+    search_terms = get_searches(infile)
+    # old_to_new_langs = {v: k for k, v in LANGUAGES.items()}
+    # search_terms = sorted(
+    #     {
+    #         search.lower().strip().replace(")", "").replace(")", "").replace(":", "")
+    #         for search in searches
+    #         # for search in c_search.split()
+    #     }
+    # )
+    found_pages = [
+        (search, search_index[search[0]])
+        for search in search_terms
+        if search[0] in search_index
     ]
 
-    langs = sorted(
-        {
-            lang
-            for result in results
-            for lang in result.keys()
-            if lang != termwiki_language
-        }
-    )
-    langs.insert(0, termwiki_language)
+    for result in found_pages:
+        print(result[0])
+        print("\n".join({page.title for page in result[1]}))
+        print()
+    print(f"Found {len(set({result[0][2] for result in found_pages}))} pages")
+    # results = [
+    #     {
+    #         language: (
+    #             f'{", ".join(termwiki_page.get_terms(language))}'
+    #             f" {termwiki_page.get_definition(language)}"
+    #         ).strip()
+    #         for termwiki_page in termwiki_pages
+    #         for language in termwiki_page.get_languages()
+    #     }
+    #     for termwiki_pages in [
+    #         search_index[search] for search in search_terms if search in search_index
+    #     ]
+    # ]
+    # langs = sorted(
+    #     {
+    #         lang
+    #         for result in results
+    #         for lang in result.keys()
+    #         if lang != termwiki_language
+    #     }
+    # )
+    # langs.insert(0, termwiki_language)
 
-    click.echo(f"Writing to {outfile}")
-    with click.open_file(outfile, "w") as f:
-        print("\t".join(old_to_new_langs.get(old) for old in langs), file=f)
-        print(
-            "\n".join(
-                [
-                    "\t".join([result.get(lang, "") for lang in langs])
-                    for result in results
-                ]
-            ),
-            file=f,
-        )
+    # click.echo(f"Writing to {outfile}")
+    # with click.open_file(outfile, "w") as f:
+    #     print("\t".join(old_to_new_langs.get(old) for old in langs), file=f)
+    #     print(
+    #         "\n".join(
+    #             [
+    #                 "\t".join([result.get(lang, "") for lang in langs])
+    #                 for result in results
+    #             ]
+    #         ),
+    #         file=f,
+    #     )
